f72a618dd92d5f51a327b37c68406ec4f2234343
Mathieu Zhang <mathieu@mobiusmed.com>
2017-09-23 19:47:12 -0700

    More style changes. Using markdown for highlighting.

--- pylint/1675/README.md
+++ pylint/1675/README.md
@@ -1,23 +1,20 @@
 Reproduction:
 
-```sh
+```markdown
+/home/mlfzhang/Projects/pylint/pylint/1675/pkga/pkga/__init__.pyc
+/home/mlfzhang/Projects/pylint/pylint/1675/pkgb-ns/pkgb/__init__.py
 No config file found, using default configuration
 ************* Module pkga.pylint_error
 E:  2, 0: No name 'FOO' in module 'pkga.pkgb' (no-name-in-module)
-^ ^ ^ ^ ^
-pylint is incorrectly trying to import from module pkga.pkgb.
-NOTE the same error does not appear for the next line in the same file, which uses `from pkgb import *` wildcard import.
+## pylint is incorrectly trying to import from module pkga.pkgb.
+## NOTE the same error does not appear for the next line in the same file, which uses `from pkgb import *` wildcard import.
 
 No config file found, using default configuration
-^ ^ ^ ^ ^
-OK if we are not using the do-nothing plugin.
+## OK if we are not using the do-nothing plugin.
 
 No config file found, using default configuration
-^ ^ ^ ^ ^
-OK also if pkgb is not namespaced.
+## OK also if pkgb is not namespaced.
 
-/home/mlfzhang/Projects/pylint/pylint/1675/pkga/pkga/__init__.pyc
-/home/mlfzhang/Projects/pylint/pylint/1675/pkgb-ns/pkgb/__init__.py
 running develop
 Removing /home/mlfzhang/.virtualenv/m3d/lib/python2.7/site-packages/Package-A.egg-link (link to .)
 Removing Package-A 0.0.0 from easy-install.pth file
--- pylint/1675/test.sh
+++ pylint/1675/test.sh
@@ -15,32 +15,6 @@ trap cleanup EXIT
 	( cd pkgb-ns; python setup.py develop )
 } > /dev/null
 
-pylint --load-plugins=pkga.astng_transform --errors-only pkga/pkga pkgb-ns/pkgb # FAIL
-
-cat << EOF
-^ ^ ^ ^ ^
-pylint is incorrectly trying to import from module pkga.pkgb.
-NOTE the same error does not appear for the next line in the same file, which uses \`from pkgb import *\` wildcard import.
-
-EOF
-
-pylint --errors-only pkga/pkga pkgb-ns/pkgb # OK
-
-cat << EOF
-^ ^ ^ ^ ^
-OK if we are not using the do-nothing plugin.
-
-EOF
-
-sed -e '/\.declare_namespace/d' -i pkgb-ns/pkgb/__init__.py
-pylint --rcfile=pylintrc --errors-only pkga/pkga pkgb-ns/pkgb # OK
-cat << EOF
-^ ^ ^ ^ ^
-OK also if pkgb is not namespaced.
-
-EOF
-git co -- pkgb-ns/pkgb/__init__.py
-
 # imports are OK
 python << EOF
 
@@ -53,3 +27,30 @@ print pkgb.__file__
 from pkgb import FOO # OK
 
 EOF
+
+# Test pylint:
+
+pylint --load-plugins=pkga.astng_transform --errors-only pkga/pkga pkgb-ns/pkgb # FAIL
+
+cat << EOF
+## pylint is incorrectly trying to import from module pkga.pkgb.
+## NOTE the same error does not appear for the next line in the same file, which uses \`from pkgb import *\` wildcard import.
+
+EOF
+
+pylint --errors-only pkga/pkga pkgb-ns/pkgb # OK
+
+cat << EOF
+## OK if we are not using the do-nothing plugin.
+
+EOF
+
+sed -e '/\.declare_namespace/d' -i pkgb-ns/pkgb/__init__.py
+pylint --rcfile=pylintrc --errors-only pkga/pkga pkgb-ns/pkgb # OK
+cat << EOF
+## OK also if pkgb is not namespaced.
+
+EOF
+git co -- pkgb-ns/pkgb/__init__.py
+
+# eof
